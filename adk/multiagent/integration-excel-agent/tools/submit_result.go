4a323a94db75f3f90deae47d9b69a8517acf1386
xuzhaonan <xuzhaonan@bytedance.com>
2025-10-22 17:04:56 +0800

    feat: improve python runner

--- adk/multiagent/integration-excel-agent/tools/submit_result.go
+++ adk/multiagent/integration-excel-agent/tools/submit_result.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tools
 
 import (
@@ -18,7 +34,7 @@ import (
 
 var (
 	submitResultToolInfo = &schema.ToolInfo{
-		Name: "SubmitResult",
+		Name: "submit_result",
 		Desc: "When all steps are completed without obvious problems, call this tool to end the task and report the final execution results to the user.",
 		ParamsOneOf: schema.NewParamsOneOfByParams(map[string]*schema.ParameterInfo{
 			"is_success": {
